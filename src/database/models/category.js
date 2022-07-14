ee56db2995324f6f705792ec19a541289e96a085
Danillo Gonçalves Batista <danillogoncalves001@gmail.com>
2022-07-13 23:18:24 -0300

    feat: :white_check_mark: Requisito 7 finalizado.
    Foi feito o model de Category.

---
+++ src/database/models/category.js
@@ -0,0 +1,15 @@
+const CreateCategory = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING,
+  }, {
+    timestamps: false,
+    tableName: 'Categories',
+  });
+  return Category;
+};
+
+module.exports = CreateCategory;
